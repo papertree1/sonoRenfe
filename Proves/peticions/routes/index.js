9b6d500a61c5a53398804ab7a18a8fabb3a6497a
Omar Olmedo Ferrer <omarolmedoferrer@gmail.com>
2026-03-03 13:49:29 +0100

    resultats nets

--- Proves/peticions/routes/index.js
+++ Proves/peticions/routes/index.js
@@ -2,31 +2,50 @@ var express = require('express');
 var router = express.Router();
 
 const fs = require('fs');
+const { get } = require('http');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-console.log("Starting fetches...");
 
-results = "";
 // script per fer proves de peticions a la API de Renfe
-for (let i = 25000; i < 26000; i++) {
-  fetch(`https://serveisgrs.rodalies.gencat.cat/api/trains/${i}`)
-  .then(response => response.json())
-  .then(data => {
-    // add results to a string
-    results += `Train ${i}: ${JSON.stringify(data)}\n`;
-  });
-}
+getTrains();
 
-// wait for all fetches to finish and then write results to a file
-setTimeout(() => {
-  fs.writeFile('results.txt', results, (err) => {
-    if (err) throw err;
-    console.log('Results saved to results.txt');
-  });
-}, 30000);
+function getTrains() {
+  results = { data: [] };
+  counter = 0;
+
+  console.log("Starting fetches...");
+
+  for (let i = 25000; i < 26000; i++) {
+    fetch(`https://serveisgrs.rodalies.gencat.cat/api/trains/${i}`)
+      .then(response => response.json())
+      .then(data => {
+        // add results to a string
+
+        if (!JSON.stringify(data).includes(`"target":null`)) {
+          results.data.push({ id: i, data: data });
+        }
+
+        counter++;
+        console.log(counter);
+
+        if (counter == 1000) {
+          results.data.sort((a, b) => a.id - b.id);
+
+          fs.writeFile('resultsNet2.json', JSON.stringify(results), (err) => {
+            if (err) throw err;
+            console.log('Results saved to resultsNet.json');
+          });
+        }
+      })
+      .catch(error => {
+        console.error(`Error fetching train ${i}:`, error.message);
+        getTrains();
+      });
+  }
+}
 
 module.exports = router;
